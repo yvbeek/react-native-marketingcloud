bbebec3149a42822ee73654b05b912c12e1bed65
Yvo van Beek <yvo.vanbeek@nbcuni.com>
2022-06-30 17:14:15 -0400

    Update types

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -12,7 +12,7 @@ export declare class SFMCSDK {
     /**
      * Initializes the Salesforce Marketing Cloud SDK.
      */
-    static initializeSDK(): Promise<any>;
+    static initializeSDK(): Promise<boolean>;
     /**
      * Enables or disables verbose logging within the native Marketing Cloud SDK.
      * @param enabled - The value to enable / disable verbose logging
@@ -20,42 +20,42 @@ export declare class SFMCSDK {
     static setDebugLoggingEnabled(enabled: boolean): void;
     /**
      * Returns the token used to send push messages to the device.
-     * @returns A promise to the system token string.
+     * @returns A promise to the system token string or null
      */
-    static getDeviceToken(): Promise<string>;
+    static getDeviceToken(): Promise<string | null>;
     /**
      * Returns the profile id currently set on the device.
-     * @returns A promise to the current profile id.
+     * @returns A promise to the current profile id or null
      */
     static getProfileId(): Promise<string | null>;
     /**
      * Sets the profile id for the device's user.
-     * @param profileId - The value to be set as the profile id of the device's user.
+     * @param profileId - The value to be set as the profile id of the device's user
      */
     static setProfileId(profileId: string): void;
     /**
      * Sets custom profile attributes on the push notification record.
-     * @param attributes The attributes keys and values to set
+     * @param attributes - The attributes keys and values to set
      */
-    static setProfileAttributes(attributes: Record<string, string>): Promise<void>;
+    static setProfileAttributes(attributes: Record<string, string>): void;
     /**
      * Indicates if the push messaging functionality is enabled.
-     * @returns A promise to the boolean representation of whether push is enabled.
+     * @returns A promise to the boolean that indicates if push is enabled
      */
     static isPushEnabled(): Promise<boolean>;
     /**
      * Enables or disables the push messaging functionality.
-     * @param enabled - The value to enable / disable push messaging.
+     * @param enabled - The value to enable / disable push messaging
      */
     static setPushEnabled(enabled: boolean): void;
     /**
      * Returns all messages in the inbox.
-     * @returns A promise to a list of inbox messages.
+     * @returns A promise to a list of inbox messages
      */
-    static getAllMessages(): Promise<InboxMessage>;
+    static getAllMessages(): Promise<InboxMessage[]>;
     /**
      * Returns the total number of messages in the inbox.
-     * @returns A promise to the number of inbox messages.
+     * @returns A promise to the number of inbox messages
      */
     static getAllMessagesCount(): Promise<number>;
     /**
@@ -66,28 +66,29 @@ export declare class SFMCSDK {
     /**
      * Marks a single message as deleted.
      * @param messageId - The message to mark as deleted
-     * @returns A boolean indicating if the action was succesfull
+     * @returns A promise to the boolean indicating if the action was succesful
      */
     static markMessageDeleted(messageId: string): Promise<boolean>;
     /**
      * Marks a single message as read.
      * @param messageId - The message to mark as read
-     * @returns A boolean indicating if the action was succesfull
+     * @returns A promise to the boolean indicating if the action was succesful
      */
     static markMessageRead(messageId: string): Promise<boolean>;
     /**
      * Marks all messages in the inbox as deleted.
-     * @returns A boolean indicating if the action was succesfull
+     * @returns A promise to the boolean indicating if the action was succesful
      */
     static markAllMessagesDeleted(): Promise<boolean>;
     /**
      * Marks all messages in the inbox as read.
-     * @returns A boolean indicating if the action was succesfull
+     * @returns A promise to the boolean indicating if the action was succesful
      */
     static markAllMessagesRead(): Promise<boolean>;
     /**
      * Reload and refresh Inbox messages from the MarketingCloud server.
      * The underlying request to the server will be throttled such that it will execute at most every 60 seconds.
+     * @returns A promise to the boolean indicating if the refresh was succesful
      */
     static refreshMessages(): Promise<boolean>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ export class SFMCSDK {
   /**
    * Initializes the Salesforce Marketing Cloud SDK.
    */
-  static async initializeSDK() {
+  static async initializeSDK(): Promise<boolean> {
     return NativeSDK.initializeSDK()
   }
 
@@ -31,7 +31,7 @@ export class SFMCSDK {
    * Enables or disables verbose logging within the native Marketing Cloud SDK.
    * @param enabled - The value to enable / disable verbose logging
    */
-  static setDebugLoggingEnabled(enabled: boolean) {
+  static setDebugLoggingEnabled(enabled: boolean): void {
     NativeSDK.setDebugLoggingEnabled(enabled)
   }
 
@@ -39,15 +39,15 @@ export class SFMCSDK {
 
   /**
    * Returns the token used to send push messages to the device.
-   * @returns A promise to the system token string.
+   * @returns A promise to the system token string or null
    */
-  static getDeviceToken(): Promise<string> {
+  static getDeviceToken(): Promise<string | null> {
     return NativeSDK.getDeviceToken()
   }
 
   /**
    * Returns the profile id currently set on the device.
-   * @returns A promise to the current profile id.
+   * @returns A promise to the current profile id or null
    */
   static async getProfileId(): Promise<string | null> {
     return NativeSDK.getProfileId()
@@ -55,23 +55,23 @@ export class SFMCSDK {
 
   /**
    * Sets the profile id for the device's user.
-   * @param profileId - The value to be set as the profile id of the device's user.
+   * @param profileId - The value to be set as the profile id of the device's user
    */
-  static setProfileId(profileId: string) {
+  static setProfileId(profileId: string): void {
     NativeSDK.setProfileId(profileId)
   }
 
   /**
    * Sets custom profile attributes on the push notification record.
-   * @param attributes The attributes keys and values to set
+   * @param attributes - The attributes keys and values to set
    */
-  static async setProfileAttributes(attributes: Record<string, string>) {
+  static setProfileAttributes(attributes: Record<string, string>): void {
     NativeSDK.setProfileAttributes(attributes)
   }
 
   /**
    * Indicates if the push messaging functionality is enabled.
-   * @returns A promise to the boolean representation of whether push is enabled.
+   * @returns A promise to the boolean that indicates if push is enabled
    */
   static isPushEnabled(): Promise<boolean> {
     return NativeSDK.isPushEnabled()
@@ -79,9 +79,9 @@ export class SFMCSDK {
 
   /**
    * Enables or disables the push messaging functionality.
-   * @param enabled - The value to enable / disable push messaging.
+   * @param enabled - The value to enable / disable push messaging
    */
-  static setPushEnabled(enabled: boolean) {
+  static setPushEnabled(enabled: boolean): void {
     NativeSDK.setDebugLoggingEnabled(enabled)
   }
 
@@ -89,10 +89,10 @@ export class SFMCSDK {
 
   /**
    * Returns all messages in the inbox.
-   * @returns A promise to a list of inbox messages.
+   * @returns A promise to a list of inbox messages
    */
-  static async getAllMessages(): Promise<InboxMessage> {
-    const messages = await NativeSDK.getAllMessages()
+  static async getAllMessages(): Promise<InboxMessage[]> {
+    const messages: any[] = await NativeSDK.getAllMessages()
     return messages.map((message: any) => {
       const {
         id, subject, url, read, deleted,
@@ -119,7 +119,7 @@ export class SFMCSDK {
 
   /**
    * Returns the total number of messages in the inbox.
-   * @returns A promise to the number of inbox messages.
+   * @returns A promise to the number of inbox messages
    */
   static async getAllMessagesCount(): Promise<number> {
     return NativeSDK.getAllMessagesCount()
@@ -136,7 +136,7 @@ export class SFMCSDK {
   /**
    * Marks a single message as deleted.
    * @param messageId - The message to mark as deleted
-   * @returns A boolean indicating if the action was succesfull
+   * @returns A promise to the boolean indicating if the action was succesful
    */
   static async markMessageDeleted(messageId: string): Promise<boolean> {
     return NativeSDK.markMessageDeleted(messageId)
@@ -145,7 +145,7 @@ export class SFMCSDK {
   /**
    * Marks a single message as read.
    * @param messageId - The message to mark as read
-   * @returns A boolean indicating if the action was succesfull
+   * @returns A promise to the boolean indicating if the action was succesful
    */
   static async markMessageRead(messageId: string): Promise<boolean> {
     return NativeSDK.markMessageRead(messageId)
@@ -153,7 +153,7 @@ export class SFMCSDK {
 
   /**
    * Marks all messages in the inbox as deleted.
-   * @returns A boolean indicating if the action was succesfull
+   * @returns A promise to the boolean indicating if the action was succesful
    */
   static async markAllMessagesDeleted(): Promise<boolean> {
     return NativeSDK.markAllMessagesDeleted()
@@ -161,7 +161,7 @@ export class SFMCSDK {
 
   /**
    * Marks all messages in the inbox as read.
-   * @returns A boolean indicating if the action was succesfull
+   * @returns A promise to the boolean indicating if the action was succesful
    */
   static async markAllMessagesRead(): Promise<boolean> {
     return NativeSDK.markAllMessagesRead()
@@ -170,6 +170,7 @@ export class SFMCSDK {
   /**
    * Reload and refresh Inbox messages from the MarketingCloud server.
    * The underlying request to the server will be throttled such that it will execute at most every 60 seconds.
+   * @returns A promise to the boolean indicating if the refresh was succesful
    */
   static async refreshMessages(): Promise<boolean> {
     return NativeSDK.refreshMessages()
